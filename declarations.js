d78e1d4305dca51f834547749b088ce97ee86d94
fahdaguenouz <faguenouz@gmail.com>
2025-02-19 11:13:09 +0100

    declarations

---
+++ declarations.js
@@ -0,0 +1,16 @@
+const escapeStr='\/"'
+const arr=[4,'2']
+const obj={
+    str:"fahd",
+    num:1,
+    bool:false,
+    undef:undefined
+}
+const nested={
+    arr:[4,undefined,'4'],
+    obj:{
+        str:"piscine js",
+        num:25,
+        bool:false
+    }
+}

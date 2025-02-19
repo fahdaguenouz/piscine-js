e55da9c96d672096844a0ae3601c568616aca9f0
fahdaguenouz <faguenouz@gmail.com>
2025-02-19 12:30:24 +0100

    more or less

---
+++ more-or-less.js
@@ -0,0 +1,14 @@
+function more(num){
+    return num+=1
+}
+
+function less(num){
+    return num--
+}
+
+const add=(num1,num2)=>{
+    return num1+num2
+}
+const sub=(num1,num2)=>{
+    return num2-num1
+}

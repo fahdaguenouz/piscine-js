84cd3a0a3fe28210e4987fa7f63828781b3a1e9a
fahdaguenouz <faguenouz@gmail.com>
2025-02-21 18:03:20 +0100

    nasa

---
+++ nasa.js
@@ -0,0 +1,21 @@
+function nasa(num){
+    let res=""
+
+    for(let i=1;i<=num;i++){
+        if(i%3==0&&i%5==0){
+            res+="NASA"
+        }else if (i%5==0){
+            res+="SA"
+        }else if(i%3==0){
+            res+="NA"
+        }else{
+            res+=i
+        }
+        if(i!=num){
+            res+=" "
+        }
+    }
+    return res
+}
+
+console.log(nasa(15));

1b04fbca2fbe7644201899b2db21251d8e7b7a16
Fahd Aguenouz <120320573+fahdaguenouz@users.noreply.github.com>
2025-02-22 19:33:42 +0100

    find-expression

---
+++ find-expression.js
@@ -0,0 +1,28 @@
+function findExpression(num){
+    let res=""
+    let n=num
+    const add4 ="+4"
+    const mul2 ="*2"
+    n=n-4
+    res+=add4
+    while(n>1){
+        console.log(n);
+        if(n%4==0&&n>4){
+            n=n-4
+            res+=add4
+        }else if(n%2==0){
+            n=n/2
+            res+=mul2
+        }else if(n>4){
+            n=n-4
+            res+=add4
+        }else {
+            return undefined
+        }
+    }
+    return res
+}
+
+
+console.log(findExpression(8));
+

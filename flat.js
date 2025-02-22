647ff191bccc9b6468fe6b175231077751eca9ae
Fahd Aguenouz <120320573+fahdaguenouz@users.noreply.github.com>
2025-02-22 18:45:19 +0100

    flat.

--- flat.js
+++ flat.js
@@ -1,31 +1,24 @@
-function flat(elem,num=1){
-    let c=0
-    let arr=[]
-    elem.map((e,i)=>{
-        console.log(Array.isArray(e));
-        console.log(e);
-        if(Array.isArray(e)&&c<=num){
-              //  arr.push(e)
-                //flat(e,num-c)
-            function rec(e,num){
-                e.map((elm)=>{
-                 if(!Array.isArray(elem)){
-                   arr.push(elm)
-                   c++
-                 }else{
-                   rec(elem,num)
-                 }
-               })
-            }
-            }else{
-                arr.push(e)
-
-            }
-
-
-    })
-    return arr
-
+function flat(elem,num=1,arr=[]){
+   
+   
+    for(let i=0;i<elem.length;i++){
+      if(Array.isArray(elem[i])&&num>0){
+        
+          //  console.log(elem[i]);
+            
+            //console.log(c);
+            
+             flat(elem[i],num-1,arr)
+       
+      }else{
+       // console.log("f"+elem[i]);
+        
+        arr.push(elem[i])
+      }
+    }
+  
+  
+return arr
 }
 
-console.log(flat([0, 1, [2, [3, [4, 5]]]],3));
+console.log(flat([1, [2, [3], [4, [5]]]],Infinity));

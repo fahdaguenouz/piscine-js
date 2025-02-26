5edda3fc083c6f0619a566f32295ae0e3502b206
fahdaguenouz <faguenouz@gmail.com>
2025-02-26 15:52:15 +0000

    dr-strange

--- dr-strange.js
+++ dr-strange.js
@@ -18,11 +18,29 @@ function addWeek(input){
    let myear=new Date("0001-01-01")
    let day=new Date(input)
    let year=Math.floor((((day-myear)/(1000*60*60*24)))%14)
-   console.log(year);
+   console.log(day);
    console.log(Math.floor(((year))%14));
 
    return myDates[(year)]
 }
 
+function timeTravel(obj){
+ 
+  
+  //console.log(obj);
+  obj.date.setHours(obj.hour)  
+  obj.date.setMinutes(obj.minute)  
+  obj.date.setSeconds(obj.second)  
 
-console.log(addWeek('0001-01-03'));
+
+  return obj.date
+}
+
+//console.log(addWeek('0001-01-02'));
+
+console.log(timeTravel({
+  date: new Date('2000-05-09 01:28:02'),
+  hour: 21,
+  minute: 22,
+  second: 22,
+}).toString());

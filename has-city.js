b6e882323e85b7e3600ea118a5cd1d351e34931a
Fahd Aguenouz <120320573+fahdaguenouz@users.noreply.github.com>
2025-02-28 16:39:06 +0100

    has-city

---
+++ has-city.js
@@ -0,0 +1,23 @@
+function hasCity(country,cities){
+    return function(citie){
+        for(let i=0;i<cities.length;i++){
+            if(cities[i]===citie){
+                return `${citie} is a city from ${country}`
+            }
+            
+        }
+        return `${citie} is not city from ${country}`
+        
+    }
+}
+
+const isFrench = hasCity('France', [
+    'Bordeaux',
+    'Paris',
+    'Lille',
+    'Lyon',
+    'Marseille',
+    'Saint-Étienne',
+  ])
+  console.log(isFrench('Lyos'));
+  

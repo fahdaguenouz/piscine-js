bf5112242527e68db074fe29cc9954ebce6b7c38
fahdaguenouz <faguenouz@gmail.com>
2025-03-01 18:29:36 +0000

    fifty

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -1,21 +1,35 @@
-import {colors} from './fifty-shades-of-cold.data.js'
-export function generateClasses(){
-    let bstyle=document.createElement("style")
-    for(let color of colors){
-        console.log(color);
-        
-        let cll=`.${color}{
+import { colors } from './fifty-shades-of-cold.data.js'
+export function generateClasses() {
+    let bstyle = document.createElement("style")
+    for (let color of colors) {
+
+        let cll = `.${color}{
         background: ${color};
         }`
-       bstyle.textContent+=" \n"+cll
+        bstyle.textContent += " \n" + cll
         document.head.appendChild(bstyle)
     }
-    
-}
-export function generateColdShades(){
 
 }
+export function generateColdShades() {
+    const col=colors.filter((clr)=>{
+        return clr.includes("aqua")||clr.includes("blue")||clr.includes("turquoise")||clr.includes("green")||clr.includes("cyan")||clr.includes("navy")||clr.includes("purple")
+    })
+    for (let color of col) {
+            let div = document.createElement("div")
+          div.className = color
+          div.textContent = color
+        document.body.appendChild(div)
+    }
+}
 
-export function choseShade(){
+
+
+export function choseShade(shade) {
+   const colDiv=document.querySelectorAll("div")
+   colDiv.forEach((ele)=>{
+     ele.classList.replace(ele.className,shade)
+   })
+ 
 
 }
